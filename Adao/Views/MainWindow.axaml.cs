00a03e65f048cc2e3c8974b1076c87e141098e4f
tesibrasil <vitor.avila@tesigroup.tech>
2022-06-21 22:47:52 -0300

    first commit

---
+++ Adao/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Adao.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
